77caf769e60dc8e3805d983b1d04d1178e1b5023
Nikolay K <113373360+Ne1ce@users.noreply.github.com>
2022-12-27 09:26:10 +0300

    2

--- src/main/java/ru/alishev/springcourse/models/Person.java
+++ src/main/java/ru/alishev/springcourse/models/Person.java
@@ -1,16 +1,47 @@
 package ru.alishev.springcourse.models;
 
-    public class Person {
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class Person {
         private int id;
+        @Min(value = 5,message = "Age incorrect")
+        private int age;
+        @NotEmpty(message = "Name empty")
+        @Size(min = 2,max=10,message = "Name 2-30 char")
         private String name;
+        @NotEmpty(message = "Email empty")
+        @Email(message = "Email incorrect")
+        private String email;
 
         public Person() {
 
         }
 
-        public Person(int id, String name) {
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Person(int id,String name, int age, String email) {
             this.id = id;
             this.name = name;
+            this.age = age;
+            this.email = email;
         }
 
         public int getId() {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
+<html lang="en" xmlns:th="http://www.thymeleaf.org">
 <html>
 <body>
 <h2>Hello World!</h2>
+<a href="/people"> Go to Users</a>
 </body>
 </html>
